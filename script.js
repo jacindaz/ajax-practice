f5a57574a477fbabaac76fe3db0b3cc4b9af26a8
Jacinda Zhong <jacindaz@gmail.com>
2014-08-03 19:29:09 -0400

    Selects div id, and populates with responseText from data.txt file

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ if (window.XMLHttpRequest) {
 request.open('GET', 'data.txt');
 request.onreadystatechange = function() {
     if ((request.readyState===4) && (request.status===200)) {
-        console.log(request);
-        document.writeln(request.responseText);
+        var modify = document.getElementById('update');
+        // innerHTML is the element inside the selected elemtn
+        // which in this case, is the 'update' id
+        modify.innerHTML = request.responseText;
     }
 }
 request.send();
