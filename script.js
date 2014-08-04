2a86b0a198962cd8867909fd1ec7d57d02225415
Jacinda Zhong <jacindaz@gmail.com>
2014-08-03 20:24:27 -0400

    Load info from file via 'load' button

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-var request;
+function loadAJAX() {
+    var request;
 
-if (window.XMLHttpRequest) {
-    request = new XMLHttpRequest();
-} else {
-    // this line supports AJAX in older browsers
-    request = new ActiveXObject("Microsoft.XMLHTTP");
-}
-
-request.open('GET', 'data.json');
-request.onreadystatechange = function() {
-    if ((request.readyState===4) && (request.status===200)) {
-        var items = JSON.parse(request.responseText);
-        console.log(items);
-        var output = '<ol>';
-
-        for (var key in items) {
-            output += '<li>' + items[key].reknown + '</li>';
-            console.log("Key: " + key);
-        }
-        output += '</ol>';
-        document.getElementById('update').innerHTML = output;
+    if (window.XMLHttpRequest) {
+        request = new XMLHttpRequest();
+    } else {
+        // this line supports AJAX in older browsers
+        request = new ActiveXObject("Microsoft.XMLHTTP");
     }
+
+    request.open('GET', 'data.json');
+    request.onreadystatechange = function() {
+        if ((request.readyState===4) && (request.status===200)) {
+            var items = JSON.parse(request.responseText);
+            console.log(items);
+            var output = '<ol>';
+
+            for (var key in items) {
+                output += '<li>' + items[key].reknown + '</li>';
+                console.log("Key: " + key);
+            }
+            output += '</ol>';
+            document.getElementById('update').innerHTML = output;
+        }
+    }
+    request.send();
 }
-request.send();
