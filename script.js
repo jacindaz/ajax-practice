afd791c99f7d609279bd619b6731f615727c664d
Jacinda Zhong <jacindaz@gmail.com>
2014-08-03 22:14:26 -0400

    Adds jQuery, sample selecting class or id

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-var mybutton = document.getElementById('loadbutton');
-mybutton.onclick = function() {
-    var request;
+// select div with id update
+$('#update').load('data.txt');
 
-    if (window.XMLHttpRequest) {
-        request = new XMLHttpRequest();
-    } else {
-        // this line supports AJAX in older browsers
-        request = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-
-    request.open('GET', 'data.json');
-    request.onreadystatechange = function() {
-        if ((request.readyState===4) && (request.status===200)) {
-            var items = JSON.parse(request.responseText);
-            console.log(items);
-            var output = '<ol>';
-
-            for (var key in items) {
-                output += '<li>' + items[key].reknown + '</li>';
-                console.log("Key: " + key);
-            }
-            output += '</ol>';
-            document.getElementById('update').innerHTML = output;
-        }
-    }
-    request.send();
-}
+// select div with class test_class
+$('.test_class').load('data.txt');
